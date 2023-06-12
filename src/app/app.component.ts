6fb57c8f8c640782b239bb5f863eae52023e7aa5
Uladzislau Kachurka <Uladzislay_Kachurka@epam.com>
2023-06-12 21:52:17 +0300

    init: project set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'sv-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'svim-ui';
-}
+export class AppComponent {}
